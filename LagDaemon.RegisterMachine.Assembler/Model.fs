47da939f545ffacaa0817fc7626e35994eaca48b
William Westlake <wwestlake@lagdaemon.com>
2019-10-23 14:52:11 -0400

    setup some parsers

---
+++ LagDaemon.RegisterMachine.Assembler/Model.fs
@@ -0,0 +1,19 @@
+﻿namespace LagDaemon.RegisterMachine.Assembler
+
+module Model =
+    open FParsec
+
+    type UserState = unit
+    type Parser<'t> = Parser<'t, UserState>
+
+    let ws = spaces
+    let ws1 = spaces1
+    let str = pstringCI
+
+    let pnum : Parser<_> = pint64
+
+    let pnop : Parser<_> = str "nop"
+    let preg : Parser<_> = str "r" >>. pnum
+    let ppush : Parser<_> = str "push" >>. ws1 >>. preg
+    let ppop : Parser<_> = str "pop" >>. ws1 >>. preg
+    let pload : Parser<_> = str "load" >>. ws1 >>. preg >>. pnum
---
+++ LagDaemon.RegisterMachine.Core/Model.fs
@@ -0,0 +1,97 @@
+﻿namespace LagDaemon.RegisterMachine.Core
+
+module Model =
+
+
+    type Machine = {
+        IP: int
+        BP: int
+        SP: int
+        R: int array
+        CF: bool
+        Z: bool
+        Eq: int
+        Memory: int array
+        Ports: int array
+    }
+        with 
+            static member Init size ports = 
+                {
+                    IP = 0
+                    BP = 0
+                    SP = 0
+                    CF = false
+                    Z = false
+                    Eq = 0
+                    R = [|0;0;0;0;0;0;0;0;0;0;0;0;0;0;0|]
+                    Memory = [| for i in [0..size] -> 0 |]
+                    Ports = [| for i in [0..ports] -> 0 |]
+                }
+    
+            member this.incrementIP () =
+                {
+                    this with IP = this.IP + 2
+                }
+
+            member this.jump addr =
+                {
+                    this with IP = addr
+                }
+
+            member this.incrementSP () =
+                {
+                    this with SP = this.SP + 1
+                }
+
+            member this.decrementSP () =
+                {
+                    this with SP = this.SP - 1
+                }
+
+            member this.setBasePointer addr =
+                {
+                    this with BP = addr
+                }
+
+            member this.setRegister r value =
+                Array.set this.R r value
+
+            member this.setMemory addr value =
+                Array.set this.Memory addr value
+
+            member this.fetch () =
+                this.Memory.[this.IP]
+
+            member this.fetchData () =
+                this.Memory.[this.IP + 1]
+
+            member this.readPort port =
+                this.Ports.[port]
+
+            member this.writePort port value =
+                this.Ports.[port] = value
+
+    type OpCodes =
+        | NOP = 000x0
+        | PUSH = 100x0
+        | POP = 20
+        | ADD = 30
+        | SUB = 40
+        | MUL = 50
+        | DIV = 60
+        | JMP = 70
+        | JLE = 80
+        | JGE = 90
+        | JL = 100
+        | JG = 110
+        | JNE = 120
+        | JZ  = 130
+        | JNZ = 140
+        | LOAD = 150
+        | STORE = 160
+        | MOVE = 170
+        | AND = 180
+        | OR = 190
+        | NOT = 200
+        | XOR = 210
+        
